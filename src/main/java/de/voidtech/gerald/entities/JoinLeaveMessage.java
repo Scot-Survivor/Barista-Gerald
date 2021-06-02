c057d1942a601cc53e47f9445d3bf72ab69f581d
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-06-02 14:53:47 +0100

    Add member public. To say whether to also post a public message or not.

--- src/main/java/de/voidtech/gerald/entities/JoinLeaveMessage.java
+++ src/main/java/de/voidtech/gerald/entities/JoinLeaveMessage.java
@@ -29,19 +29,23 @@ public class JoinLeaveMessage {
 
 	@Column
 	private boolean memberDM;
+
+	@Column
+	private boolean memberPublic;
 	
 	@Deprecated
 	//ONLY FOR HIBERNATE, DO NOT USE
 	JoinLeaveMessage() {
 	}
 	
-	public JoinLeaveMessage(long serverID, String channelID, String joinMessage, String leaveMessage, boolean memberDM)
+	public JoinLeaveMessage(long serverID, String channelID, String joinMessage, String leaveMessage, boolean memberDM, boolean memberPublic)
 	{
 		this.serverID = serverID;
 		this.channelID = channelID;
 	    this.joinMessage = joinMessage;
 	    this.leaveMessage = leaveMessage;
 	    this.memberDM = memberDM;
+	    this.memberPublic = memberPublic;
 	}
 	
 	public void setServerID(long serverID) {
@@ -60,10 +64,18 @@ public class JoinLeaveMessage {
 		this.memberDM = memberDM;
 	}
 
-	public boolean getMemberDM() {
+	public boolean isMemberDM() {
 		return memberDM;
 	}
-	
+
+	public void setMemberPublic(boolean memberPublic) {
+		this.memberPublic = memberPublic;
+	}
+
+	public boolean isMemberPublic() {
+		return memberPublic;
+	}
+
 	public String getChannelID() {
 		return channelID;
 	}
